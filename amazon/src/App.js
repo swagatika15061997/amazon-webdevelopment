d15362a75b0cbff1f05e5739ed42c8ce3066a33b
swagatika <swagatika.parida2017@gmail.com>
2024-05-09 17:34:59 +0530

    v2

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 function App() {
   return (
     <div>
       <header>
-        <div className="a_header_top"></div>
+        <div className="a_header_top p-1">
+          <button className="me-1 btn h-100" style={{'width':'10%'}}>
+            <img className="img-fluid" src="./logo1.jpg" alt="" />
+          </button>
+          <button className="lh-1 text-white me-1 btn h-100 fs-6" style={{'width':'10%'}}>
+            Deliver To Swag 
+            <FontAwesomeIcon icon={faLocationDot} />
+            <span className="fw-bold d-block"><span className="a_city">Puri</span><span className="a_zip">752001</span></span>
+          </button>
+          <form action="" className="h-form bg-white w-50 h-75 d-inline-block">C</form>
+          <button className="me-1 btn h-100" style={{'width':'6%'}}>D</button>
+          <button className="me-1 btn h-100" style={{'width':'8%'}}>E</button>
+          <button className="me-1 btn h-100" style={{'width':'8%'}}>F</button>
+          <button className="me-1 btn h-100" style={{'width':'8%'}}>G</button>
+        </div>
         <div className="a_header_bottom"></div>
       </header>
       <div className="a_main position-absolute">
@@ -12,28 +27,28 @@ function App() {
           <div id="carouselExample" className="carousel slide">
             <div className="carousel-inner">
               <div className="carousel-item active">
-                <img src="images/slider/slider1.jpg" className="d-block w-100" alt="..." />
+                <img src="images/slider/slider1.jpg" className="d-block w-100" alt="..."/>
               </div>
               <div className="carousel-item">
-                <img src="images/slider/slider2.jpg" className="d-block w-100" alt="..." />
+                <img src="images/slider/slider2.jpg" className="d-block w-100" alt="..."/>
               </div>
               <div className="carousel-item">
-                <img src="images/slider/slider3.jpg" className="d-block w-100" alt="..." />
+                <img src="images/slider/slider3.jpg" className="d-block w-100" alt="..."/>
               </div>
               <div className="carousel-item">
-                <img src="images/slider/slider4.jpg" className="d-block w-100" alt="..." />
+                <img src="images/slider/slider4.jpg" className="d-block w-100" alt="..."/>
               </div>
               <div className="carousel-item">
-                <img src="images/slider/slider5.jpg" className="d-block w-100" alt="..." />
+                <img src="images/slider/slider5.jpg" className="d-block w-100" alt="..."/>
               </div>
               <div className="carousel-item">
-                <img src="images/slider/slider6.jpg" className="d-block w-100" alt="..." />
+                <img src="images/slider/slider6.jpg" className="d-block w-100" alt="..."/>
               </div>
               <div className="carousel-item">
-                <img src="images/slider/slider7.jpg" className="d-block w-100" alt="..." />
+                <img src="images/slider/slider7.jpg" className="d-block w-100" alt="..."/>
               </div>
               <div className="carousel-item">
-                <img src="images/slider/slider8.jpg" className="d-block w-100" alt="..." />
+                <img src="images/slider/slider8.jpg" className="d-block w-100" alt="..."/>
               </div>
             </div>
             <button className="carousel-control-prev h-50" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
@@ -47,63 +62,8 @@ function App() {
           </div>
         </div>
         <div className="ml-1 position-absolute a_main_bottom">
-          <div className="a_main_bottom_1 row m-0 mb-3">
-            <div className="col">A</div>
-            <div className="col">B</div>
-            <div className="col">C</div>
-            <div className="col">D</div>
-          </div>
-          <div className="a_main_bottom_2 row m-0 mb-3">
-            <div className="col-6">A</div>
-            <div className="col-3">B</div>
-            <div className="col-3">C</div>
-          </div>
-          <div className="a_main_bottom_3 p-3">
-            <div className="">
-              <h3 className="float-start">Today's Deals</h3><button className="btn btn-link float-start">see all deals</button>
-            </div>
-            <div id="carouselExample1" className="carousel slide">
-              <div className="carousel-inner text-center">
-                <div className="carousel-item active">
-                  <div className="imgCont row">
-                    <div className="col">1</div>
-                    <div className="col">2</div>
-                    <div className="col">3</div>
-                    <div className="col">4</div>
-                    <div className="col">5</div>
-                    <div className="col">6</div>
-                  </div>
-                </div>
-                <div className="carousel-item">
-                  <div className="imgCont row">
-                    <div className="col">7</div>
-                    <div className="col">8</div>
-                    <div className="col">9</div>
-                    <div className="col">10</div>
-                    <div className="col">11</div>
-                    <div className="col">12</div>
-                  </div>
-                </div>
-                <div className="carousel-item">
-                  <div className="imgCont row">
-                    <div className="col">13</div>
-                    <div className="col">14</div>
-                    <div className="col">15</div>
-                    <div className="col">16</div>
-                    <div className="col">17</div>
-                    <div className="col">18</div>
-                  </div>
-                </div>
-              </div>
-              <button className="s_cc carousel-control-prev rounded-end" type="button" data-bs-target="#carouselExample1" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-              </button>
-              <button className="s_cc carousel-control-next rounded-start" type="button" data-bs-target="#carouselExample1" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-              </button>
-            </div>
+          <div className="a_main_bottom_1 row">
+            <div className=""></div>
           </div>
 
         </div>
